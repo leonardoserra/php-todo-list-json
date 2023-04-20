4297d08166765e64975d693fe5989200d061c8b8
leonardoserra <serra.leonardo92@gmail.com>
2023-04-20 14:54:29 +0200

    creo lista e la mando codificata in json

---
+++ server.php
@@ -0,0 +1,19 @@
+<?php
+//creo hardcode lista di valori
+$todoList = [
+    'fai la spesa',
+    'allenati',
+    'chiama la mamma',
+    'compra regalo per Sara'
+];
+
+//comunico al browser che tipo di intestazione riceverà al campo
+//Content-Type e setto application/json
+
+header('Content-Type: application/json');
+
+//in output mando il contenuto della variabile $todoList codificandolo
+//in una stringa con la funzione json_encode
+echo json_encode($todoList);
+
+?>
